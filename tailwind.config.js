983e61b63738c95d1d98d84c945a3317ede0c48d
Ashaolu John <timiasha@gmail.com>
2022-11-17 20:59:46 -0800

    sidebar snippets

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255,255,255,0.17)"
+      }
+    },
+  },
+  plugins: [],
+}
